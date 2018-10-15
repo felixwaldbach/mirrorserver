bb27e12b23d3a3e88a8de3971199d151a6e540b4
Emre Besogul <emre@Emres-MacBook-Pro.local>
2018-10-15 09:44:42 +0200

    Update Clock widget with current date

--- client/src/widgets/ClockWidget.js
+++ client/src/widgets/ClockWidget.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 
 import Clock from 'clock-react';
 
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurdsay", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 class ClockWidget extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          style: props.style
+          style: props.style,
+          time: new Date().toLocaleString(),
+          weekday: weekdays[new Date().getDay()],
+          month: new Date().getMonth(),
+          year: new Date().getFullYear()
         }
     }
 
@@ -15,10 +22,33 @@ class ClockWidget extends Component {
         response: ''
     };
 
+    componentDidMount() {
+      this.intervalID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+
+    tick() {
+      this.setState({weekday: new Date().getDay()}); // Sunday - Saturday : 0 - 6
+      this.setState({date: new Date().getUTCDate()});
+      if(this.state.month < 10) {
+        this.setState({month: "0" + new Date().getMonth()});
+      } else {
+        this.setState({month: new Date().getMonth()});
+      }
+      this.setState({year: new Date().getFullYear()});
+    }
+
     render() {
         return (
             <div>
-              <Clock style={this.state.style}/>
+              <h1><Clock style={this.state.style} /></h1>
+              <h2>{weekdays[this.state.weekday]}, {this.state.date}.{this.state.month}.{this.state.year}</h2>
             </div>
         );
     }
