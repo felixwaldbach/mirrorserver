b53435db1361367c811ea392d08119683f0cc9e3
Emre Besogul <emre@Emres-MacBook-Pro.local>
2018-11-20 16:55:23 +0100

    Fix bug to write in correct database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var io = require('socket.io')(http);
 var shell = require('shelljs');
 
 const currentUser = "Emre";
-const mongoURL = 'mongodb://127.0.0.1:27017/';
+const mongoURL = 'mongodb://127.0.0.1:27017/smartmirror';
 
 require('dotenv').load();
 
