a639a59b08c8660bb27628cd8f5b79ed1c81ec15
Emre Besogul <emre@Emres-MBP.fritz.box>
2019-01-27 14:28:51 +0100

    fix?

---
+++ client/src/widgets/ToDoWidget.js
@@ -0,0 +1,125 @@
+/*
+First: Each user has to get a accessToken and clientID by registering his Smart Mirror on: https://www.wunderlist.com/login
+Ass application url: www.google.de should be okay
+Set Header: X-Access-Token: OAUTH-TOKEN X-Client-ID: CLIENT-ID
+Endpoint to get tasks of a list: a.wunderlist.com/api/v1/tasks?list_id=INTEGER_ID_OF_List
+
+1. get all lists with id of each list
+2. get tasks of the wished lists with the id of the list
+3. print all tasks or the first 10 tasks...
+*/
+
+import React, {Component} from 'react';
+import WunderlistSDK from 'wunderlist';
+import socketIOClient from "socket.io-client";
+import frontendConfig from '../frontendConfig';
+
+var mylist = [];
+
+class ToDoWidget extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        wunderlist_settings: [],
+        lists: [],
+        mylist: [],
+        tasks: [],
+        list_id: 0,
+        endpoint: frontendConfig.server_address + ':' + frontendConfig.socket_server_port
+      }
+
+  }
+
+  componentDidMount() {
+      // WebSockets
+      this.socket = socketIOClient(this.state.endpoint);
+      this.socket.emit('send_wunderlist_settings', {
+          message: "send me credentials please!"
+      });
+
+      this.socket.on('wunderlist_settings', function (data) {
+          addListToUI(data);
+      });
+
+      this.intervalID = setInterval( () => {
+          this.socket.emit('wunderlist_settings', {
+              message: "send me credentials please!"
+          })},
+          3600000 // 1 hour = 3600 seconds = 3600000 milliseconds
+      );
+
+      const addListToUI = data => {
+          if(data) {
+            this.setState({wunderlist_settings: data});
+
+            var wunderlistAPI = new WunderlistSDK({
+              'accessToken': this.state.wunderlist_settings.client_secret,
+              'clientID': this.state.wunderlist_settings.client_id
+            });
+            //this method gets all lists
+            wunderlistAPI.http.lists.all()
+              .done(function (lists) {
+                this.setState({lists: lists});
+
+                let x;
+                for(x in lists) {
+                  if(lists[x].title === this.state.wunderlist_settings.todo_list) {
+                    this.setState({list_id: lists[x].id});
+                    // with list_id we get list items...
+                    this.getSubtasks();
+                  }
+                }
+
+
+              }.bind(this))
+              .fail(function () {
+                console.error('there was a problem');
+              });
+          }
+      };
+  }
+
+  async getSubtasks () {
+    console.log("getSubtasks");
+    let accessToken = this.state.wunderlist_settings.client_secret;
+    let client_id = this.state.wunderlist_settings.client_id;
+    fetch("https://a.wunderlist.com/api/v1/tasks?list_id="+this.state.list_id, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          "X-Access-Token": accessToken,
+          "X-Client-ID": client_id
+        },
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      this.setState({mylist: response});
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+
+  render() {
+    mylist = this.state.mylist;
+    mylist.slice(0, 5);
+    return (
+        <div className="todo-container">
+          {mylist.map((item, index) => {
+              return(
+                <div key={index}>
+                  {index < 6 ? <li>{item.title}</li>: null}
+                </div>
+              )
+            }
+          )}
+        </div>
+    );
+  }
+}
+
+export default ToDoWidget;
---
+++ database.js
@@ -0,0 +1,290 @@
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid/v4');
+const SHA256 = require('crypto-js/sha256');
+const ObjectId = require('mongodb').ObjectId;
+
+const funcall = module.exports = {
+
+  //----------------------Register user----------------------//
+  registerUser: function (db, newUserData, res, client) {
+    let username = newUserData.username;
+    let password = newUserData.password;
+
+    // Check for empty or blank entries
+    if (username.trim().length !== 0 && password.trim().length !== 0 && username !== null && password !== null) {
+
+      // Check is username is taken
+      db.collection('users').findOne({"username": username}, (err, docs) => {
+          if (err) throw err;
+          if (docs) {
+            res.send(JSON.stringify({
+                status: false,
+                message: "This username is not available. Please try another one."
+            }));
+            client.close();
+          } else {
+
+            // Add new user to database with hashed password
+            db.collection('users').insertOne({
+                "username": username,
+                "password": SHA256(password).words,
+                "face_image": ""
+            }, function (err, result) {
+                if(err) {
+                  console.log(err);
+                  res.send(JSON.stringify({
+                    status: false,
+                    message: "Something went wrong registering this user. Please try again!"
+                  }));
+                } else {
+                    console.log("User created: " + username);
+                    res.send(JSON.stringify({
+                      status: true,
+                      message: "Your user registration was successful. Please go to Login!"
+                    }));
+                }
+            });
+          }
+      });
+
+    } else {
+        res.send(JSON.stringify({
+            status: false,
+            message: "User data can't be empty."
+        }));
+        client.close();
+      }
+  },
+
+  signInUser: function(db, userData, res, client) {
+    let username = userData.username;
+    let password = userData.password;
+
+    // Check for empty fields
+    if (!username.trim() || !password) {
+        res.send(JSON.stringify({
+          status: false,
+          message: "All fields are required."
+        }));
+    } else {
+        // Check for blank fields
+        if (username != null && password != null) {
+            db.collection('users').findOne({"username": username}, (err, docs) => {
+                if (err) {
+                  console.log(username + " failed to login.");
+                  res.send(JSON.stringify({
+                      status: false,
+                      message: "Sorry, your password is incorrect. Please check again!"
+                  }));
+                  client.close();
+                  throw err;
+                }
+                if (docs) {
+                  //console.log(SHA256(password).words);
+                  //console.log(docs.password);
+                  // Check if account password of username is right
+                  if (JSON.stringify(SHA256(password).words) === JSON.stringify(docs.password)) {
+                    jwt.sign({
+                      exp: Math.floor(Date.now() / 1000) + (60 * 60 * 60 * 60 * 24),
+                      userid: docs._id,
+                      username: docs.username
+                  }, process.env.secretkey, (err, token) => {
+                      console.log(docs.username + " has logged in successfully.");
+                      res.send(JSON.stringify({
+                          status: true,
+                          token: token,
+                          message : "Correct credentials",
+                      }));
+                      client.close();
+                    });
+                  } else {
+                      console.log(username + " failed to login.")
+                      res.send(JSON.stringify({
+                          status: false,
+                          message: "Sorry, your password is incorrect. Please check again."
+                      }));
+                      client.close();
+                  }
+                }
+                else {
+                  console.log(username + " failed to login.");
+                  res.send(JSON.stringify({
+                      status: false,
+                      message: "Sorry, your password is incorrect. Please check again."
+                  }));
+                  client.close();
+                }
+            });
+        }
+    }
+  },
+
+  //----------------------Get Current User----------------------//
+  getUserDataForCurrentUser: function(db, res, userid, client) {
+    // Return logged in user information with token of jwt
+    db.collection('users').findOne({"_id": new ObjectId(userid)}, (err, res_find_user) => {
+        if (err) {
+          res.send(JSON.stringify({
+            message: "User not found"
+          }));
+          client.close();
+          throw err;
+        }
+        if (res_find_user) {
+          res.send(JSON.stringify({
+            status: true,
+            userid: userid,
+            username: res_find_user.username,
+            face_image: res_find_user.face_image
+          }));
+          client.close();
+        }
+        else {
+          res.send(JSON.stringify({
+            status: false,
+            message: "User not found"
+          }));
+          client.close();
+        }
+    })
+  },
+
+  //----------------------Update user face image----------------------//
+  uploadImageToServer: function (db, res, fileData, userId, client) {
+    db.collection("users").updateOne({ _id : new ObjectId(userId) },
+        {
+            $set: { face_image: fileData.filename }
+        }, (err, response) => {
+          if (err) {
+            throw err;
+          } else {
+            if(response.result.ok === 1) {
+              console.log("Image uploaded successfully for user: " + userId);
+              res.send(JSON.stringify({
+                status: true,
+                message: "Image uploaded successfully!"
+              }));
+            } else {
+              console.log("Updating image failed!");
+              res.send(JSON.stringify({
+                status: false,
+                message: "Updating image failed. Please try again!"
+              }));
+            }
+            client.close();
+          }
+    });
+  },
+
+  //----------------------Get Wunderlist settings from current user----------------------//
+  uploadWunderlistSettings: function (db, settings, res, userId, client) {
+    let todo_list = settings.todoList;
+    let client_secret = settings.wl_access_token;
+    let client_id = settings.wl_client_id;
+
+    // Check for empty or blank entries
+    if (todo_list.trim().length !== 0 && client_secret.trim().length !== 0 && client_id.trim().length !== 0 && todo_list !== null && client_secret !== null && client_id !== null) {
+      // Check if has already settings
+
+      db.collection('wunderlist').findOne({"user_id": new ObjectId(userId)}, (err, docs) => {
+          if (err) throw err;
+          if (docs) {
+            // UPDATE current settings
+            db.collection("wunderlist").updateOne({"user_id": new ObjectId(userId)},
+                {
+                    $set: { todo_list: todo_list, client_secret: client_secret, client_id: client_id}
+                }, (err, response) => {
+                  if (err) {
+                    throw err;
+                  } else {
+                    if(response.result.ok === 1) {
+                      res.send(JSON.stringify({
+                        status: true,
+                        message: "Wunderlist Settings updated successfully!"
+                      }));
+                    } else {
+                      res.send(JSON.stringify({
+                        status: false,
+                        message: "Error updating Wunderlist Settings. Please try again!"
+                      }));
+                    }
+                    client.close();
+                  }
+            });
+          } else {
+            // Add new entry
+            db.collection('wunderlist').insertOne({
+                "user_id": new ObjectId(userId),
+                "todo_list": todo_list,
+                "client_secret": client_secret,
+                "client_id": client_id
+            }, function (err, result) {
+                if(err) {
+                  console.log(err);
+                  res.send(JSON.stringify({
+                    status: false,
+                    message: "Something went wrong. Please try again!"
+                  }));
+                } else {
+                    res.send(JSON.stringify({
+                      status: true,
+                      message: "Wunderlist Settings added successfully"
+                    }));
+                    client.close();
+                }
+            });
+          }
+      });
+    } else {
+      res.send(JSON.stringify({
+          status: false,
+          message: "Wunderlist Settings and lists can't be empty."
+      }));
+      client.close();
+    }
+
+  },
+
+  //----------------------Get Wunderlist settings from current user----------------------//
+  getWunderlistSettings: function (db, res, userid, client) {
+
+    // JOIN in MongoDB
+    db.collection('wunderlist').aggregate([
+      { $lookup:
+        {
+          from: "users",
+          localField: "user_id",
+          foreignField: "_id",
+          as: "user"
+        }
+      },
+      { $project:
+          {
+            "todo_list": 1,
+            "client_secret": 1,
+            "client_id": 1,
+            "username": "$user.username",
+            "face_image": "$user.face_image",
+          }
+      }
+    ]).toArray((err_settings, res_settings) => {
+        if (err_settings) throw err_settings;
+        res_settings.map(item => {
+          item.todo_list = item.todo_list;
+          item.client_secret = item.client_secret;
+          item.client_id = item.client_id;
+          item.face_image = item.face_image[0];
+          item.username = item.username[0];
+        });
+        res.send(JSON.stringify({
+          status: true,
+          settings: res_settings[0]
+        }));
+        client.close();
+    });
+
+
+  },
+
+
+}
