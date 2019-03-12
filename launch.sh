d16dc6b6535355cba72f6baff51340dcb4b95511
Emre Besogul <emre@Emres-MacBook-Pro.local>
2019-03-12 09:25:49 +0100

    Update gitignore

--- launch.sh
+++ launch.sh
@@ -1,53 +1,64 @@
 #!/bin/bash
 
-echo Starting Configuration Script for Smart Mirror
+echo "Starting Configuration Script for Smart Mirror..."
 
-# change to directory where the code is based...
+
+# Installations for Smart Mirror: jq, git, nodejs, mongodb, gnome-terminal, uuidgen
+
+
+# rotate display, problem: needs reboot
+# rotate_display = 3
+
+
+# change to directory where the code folder is based
 $(cd ~/Desktop)
 
-# Installations for Smart Mirror
-# Nodejs
-$(sudo apt-get update)
-$(curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash –)
-$(sudo apt-get install -y nodejs)
 
-# JQ
-$(sudo apt-get install jq)
+# check if project exists: yes = direct to that folder, no = git clone
+if [ -d "mirrorserver" ];
+then
+	echo "Project mirrorserver exists!"
+	# change to project directory
+	$(cd ~/Desktop/smartmirror)
+else
+	echo "Project mirrorserver does not exists!"
+	# clone repo (master) folder from git
+	$(git clone https://github.com/felixwaldbach/mirrorserver.git)
+	gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver -- npm install . &
+	gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver/client -- npm install . &
+	# wait till installation of packages is done...
+	sleep 2m
+fi
 
-# Git
-$(sudo apt-get install git)
-
-# MongoDB
-$(sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4)
-$(echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list)
-$(sudo apt-get update)
-$(sudo apt-get install -y mongodb-org)
 
 # check if config.json exists
 if [ -e config.json ]
 then
-	echo config file exists
+	echo "config.json exists!"
 	config="$(cat config.json | jq '.')"
 else
-	echo config file does not exist, creating now...
+	echo "config.json does not exist, creating now...!"
 	$(touch config.json)
 fi
 
-# check if file has content
+
+# check if config.json has content
 if [ -z "$config" ]
 then
-	echo config file is empty
+	echo "config.json is empty!"
 	configIsEmpty=true
 else
-	echo config file is not empty
+	echo "config.json is not empty!"
 	configIsEmpty=false
 fi
 
+
 echo $configIsEmpty
+
 
 if [ "$configIsEmpty" = true ]
 then
-	echo New Configuration Process
+	echo "Setting up new config.json..."
 	# Set static IP
 	# Not working yet, not supported???
 
@@ -61,10 +72,10 @@ then
 	ip_address_host="${ip_address_host//[[:space:]]/}"
 	ip_address_django="${ip_address_django//[[:space:]]/}"
 
+	uuid=$(uuidgen)
+
 	echo $ip_address_host
 	echo $ip_address_django
-
-	uuid=$(uuidgen)
 	echo $uuid
 
 	# write this new configuration into config.json which is created before
@@ -81,29 +92,22 @@ else
 	echo $config
 fi
 
-# MongoDB command: sudo service mongod start OR sudo service mongod stop
-echo "Start MongoDB."
+
+# Start database: sudo service mongod start OR sudo service mongod stop
+echo "Starting MongoDB..."
 mongod="sudo service mongod start"
 $mongod
 
-# Start application (React and Express Server)
-if [ -d "mirrorserver" ];
-then
-	echo project exists
-else
-	echo project does not exists
-	$(git clone https://github.com/felixwaldbach/mirrorserver.git)
-	gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver -- npm install . &
-	gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver/client -- npm install . &
-	# wait till installation of packages is done...
-	sleep 2m
-fi
 
+# Start express server and frontend
+echo "Starting Application..."
 gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver -- node server.js . &
 gnome-terminal --working-directory=/home/emre/Desktop/mirrorserver/client -- npm start . &
+
 
 # Start Browser in fullscreen
 xdg-open http://localhost:3000 &
 xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
+
 
 # EOF
